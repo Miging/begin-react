663083c90e8719f8b23ca7c8cc783b06157fb188
Miging <dnfldpden32@gmail.com>
2023-02-25 10:39:29 +0900

    'state학습'

--- src/App.js
+++ src/App.js
@@ -2,20 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
-  const name='react'
-  const style={
-    backgroundColor:'black',
-    color:'aqua',
-    fontSize:24,
-    padding:'1rem'
-  }
   return (
-    <Wrapper>
-      <Hello name='React' color='red'isSpecial={true}/>
-      <Hello  color='pink'/>
-    </Wrapper>
+    <Counter/>
   );
 }
 
