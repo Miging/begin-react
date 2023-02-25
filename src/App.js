be4c48e7ad0483001e12fffe52d3c4eeda7cdc84
Miging <dnfldpden32@gmail.com>
2023-02-25 18:01:43 +0900

    배열추가

--- src/App.js
+++ src/App.js
@@ -5,10 +5,62 @@ import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList'
+import { useRef, useState } from 'react';
+import CreateUser from './CreateUser';
 
 function App() {
+  const [inputs,setInputs]=useState({
+    username:'',
+    email:''
+  });
+  const {username,email}=inputs;
+  const onChange=e=>{
+    const {name,value}=e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  };
+  const [users,setUsers]=useState([
+    {
+        id:1,
+        username:'민혁',
+        email:'dnfldpden32@gmail.com'
+    },{
+        id:2,
+        username:'민찬',
+        email:'fdjslfjds@naver.com'
+    },{
+        id:3,
+        username:'성훈',
+        email:'fdsfjdsljl@icloud.com'
+    }
+  ]);
+  const nextId=useRef(4);
+  const onCreate=()=>{
+    const user={
+      id:nextId.current,
+      username,
+      email
+    }
+    setUsers(users.concat(user));
+
+    setInputs({
+      username:'',
+      email:''
+    });
+    nextId.current+=1;
+  }
   return (
-    <UserList/>
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}/>
+      <UserList users={users}/>
+    </>
+
   );
 }
 
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -3,31 +3,16 @@ import React from "react";
 function User({user}){
     return(
         <div>
-            <b>{user.username}</b> <span>({user.nickname})</span>
+            <b>{user.username}</b> <span>({user.email})</span>
         </div>
     )
 }
 
-function UserList(props) {
-    const users=[
-        {
-            id:1,
-            username:'민혁',
-            nickname:'미기'
-        },{
-            id:2,
-            username:'민찬',
-            nickname:'바보'
-        },{
-            id:3,
-            username:'성훈',
-            nickname:'등신'
-        }
-    ]
+function UserList({users}) {
     return(
         <div>
-            {users.map(user=>(
-                <User user={user} key={user.id}/>
+            {users.map((user,index)=>(
+                <User user={user} key={index} />
             ))}
         </div>
     )
